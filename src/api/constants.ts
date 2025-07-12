87a3a1bd701407c35d66a6e1c7c9f4f5a6c34daa
Daniil <dgilev75@gmail.com>
2025-07-12 13:13:20 +0300

    feat: setting up proxy for netlify

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_API_URL =
-  process.env.NODE_ENV === 'development' ? process.env.API_URL_DEV : process.env.API_URL_PROD;
+  process.env.NODE_ENV === 'development' ? process.env.API_URL_DEV : process.env.API_URL_DEV;
 
 export const MAX_TOTAL_ITEMS = 100;
